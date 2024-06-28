80ddfd7d8227c7065eeefe4d0f1da33898daa8ec
SS-343 <60721439+SS-343@users.noreply.github.com>
2024-06-28 01:01:41 -0400

    Create booteffect.js

---
+++ assets/js/booteffect.js
@@ -0,0 +1,33 @@
+const bootText = `
+Copland OS v.3.1
+(c)1998-2023 Tachibana Labs
+To enter recovery mode,
+press ESC...
+
+Booting kernel...
+Loading modules...ps/2.. ok
+Loading modules...usp.. ok
+Loading modules...hci.. ok
+Loading modules...net.. ok
+
+Starting Copland OS...
+
+Welcome back, User.
+Let's all love Lain.
+`;
+
+let i = 0;
+
+function typeBootText() {
+    if (i < bootText.length) {
+        document.getElementById('boot-text').innerHTML += bootText.charAt(i);
+        i++;
+        setTimeout(typeBootText, Math.random() * 10 + 20);  // Vary typing speed for effect
+    } else {
+        setTimeout(() => {
+            document.getElementById('boot-screen').style.display = 'none';  // Hide boot screen after a 1s delay
+        }, 1300);
+    }
+}
+
+window.onload = typeBootText;
